616019200fd6cfeb378806efdc6fe837246b3621
Jørgen Vatle <Jorgen@WcAServices.net>
2025-08-14 09:41:56 +0300

    Add describe block to named import test

--- packages/type-api/src/plugin/transformMethod.test.ts
+++ packages/type-api/src/plugin/transformMethod.test.ts
@@ -1,30 +1,34 @@
 import { inspect } from 'node:util';
-import { expect, it } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { transformMethod } from './transformMethod';
 
-// language="javascript"
-const code = `
-import { defineMethod } from '@meteor-vite/type-api';
-
-export const createLink = defineMethod('links.create', {
-    handler: () => {
-        return {
-            myLink: { href: 'http://example.com' }
+describe('Named type-api import', () => {
+    // language="javascript"
+    const code = // <editor-fold desc="Inline template">
+    `
+    import { defineMethod } from '@meteor-vite/type-api';
+    
+    export const createLink = defineMethod('links.create', {
+        handler: () => {
+            return {
+                myLink: { href: 'http://example.com' }
+            }
         }
-    }
-});
-
-export const updateLink = defineMethod('links.update', {
-    handler: (params) => {
-        return 'this should be omitted from client script';
-    }
-})
-`
-
-it('Can parse ASTs', () => {
-    const { AST } = transformMethod(code);
+    });
     
-    console.log(inspect(AST, { depth: 4, colors: true }));
+    export const updateLink = defineMethod('links.update', {
+        handler: (params) => {
+            return 'this should be omitted from client script';
+        }
+    })
+    `
+    // </editor-fold>
     
-    expect(AST).toBeTruthy();
+    it('Can parse ASTs', () => {
+        const { AST } = transformMethod(code);
+        
+        console.log(inspect(AST, { depth: 4, colors: true }));
+        
+        expect(AST).toBeTruthy();
+    })
 })
