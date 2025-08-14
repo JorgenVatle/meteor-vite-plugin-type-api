b0b6dcf72ca84cd4845db87c983b7aaa03d7f949
Jørgen Vatle <Jorgen@WcAServices.net>
2025-08-14 04:54:07 +0300

    Add basic link creation method

--- example/app/imports/ui/pages/index.vue
+++ example/app/imports/ui/pages/index.vue
@@ -1,3 +1,29 @@
 <template>
-  <h1>Hello, world!</h1>
+  <div>
+    <h1>Hello, world!</h1>
+    <form @submit.prevent="form.submit()">
+      <button type="submit">Create link</button>
+    </form>
+  </div>
 </template>
+
+<script lang="ts" setup>
+import { reactive } from 'vue';
+import { createLink } from '../../api';
+
+const form = reactive({
+    data: {
+        href: '',
+        text: '',
+    },
+    error: null as Error | null,
+    async submit() {
+        try {
+            await createLink(form.data);
+        } catch (error) {
+            form.error = new Error('Failed to create link', { cause: error });
+            console.error(form.error);
+        }
+    }
+})
+</script>
