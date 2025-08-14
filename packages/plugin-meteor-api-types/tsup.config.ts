22c518f9fd4acfd7f2e1c9a0440de81f95136c1f
Jørgen Vatle <Jorgen@WcAServices.net>
2025-08-14 04:37:52 +0300

    Build for browser and node platforms

--- packages/plugin-meteor-api-types/tsup.config.ts
+++ packages/plugin-meteor-api-types/tsup.config.ts
@@ -1,12 +1,30 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig({
-    entry: ['./src/index.ts'],
-    format: ['esm', 'cjs'],
-    platform: 'node',
-    skipNodeModulesBundle: true,
-    dts: true,
-    define: {
-        Meteor: 'Meteor',
+export default defineConfig([
+    {
+        entry: ['./src/index.ts'],
+        format: ['esm', 'cjs'],
+        outDir: 'dist/node',
+        platform: 'node',
+        skipNodeModulesBundle: true,
+        dts: true,
+        clean: true,
+        define: {
+            Meteor: 'Meteor',
+            __IS_SERVER__: 'true',
+        }
+    },
+    {
+        entry: ['./src/index.ts'],
+        outDir: 'dist/browser',
+        clean: true,
+        format: ['esm', 'cjs'],
+        platform: 'browser',
+        skipNodeModulesBundle: true,
+        dts: true,
+        define: {
+            Meteor: 'Meteor',
+            __IS_SERVER__: 'false',
+        }
     }
-})
+])
