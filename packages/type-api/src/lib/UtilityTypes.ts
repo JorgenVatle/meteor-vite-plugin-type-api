70774658b30efc05bec9f8c54b8a8e8e8e0c1bbd
Jørgen Vatle <Jorgen@WcAServices.net>
2025-08-14 06:58:32 +0300

    Add DeepRequired utility type

--- packages/type-api/src/lib/UtilityTypes.ts
+++ packages/type-api/src/lib/UtilityTypes.ts
@@ -1,1 +1,3 @@
-export type DeepPartial<T> = { [P in keyof T]?: DeepPartial<T[P]> }
+export type DeepPartial<T> = { [P in keyof T]?: DeepPartial<T[P]> };
+
+export type DeepRequired<T> = { [P in keyof T]-?: DeepRequired<T[P]> };
