3eeebd3fe920b67abffe6722926dfb073d4e9566
Jørgen Vatle <Jorgen@WcAServices.net>
2025-08-14 09:45:36 +0300

    Move mock code into a dedicated prop

--- packages/type-api/src/plugin/transformMethod.test.ts
+++ packages/type-api/src/plugin/transformMethod.test.ts
@@ -2,33 +2,38 @@ import { inspect } from 'node:util';
 import { describe, expect, it } from 'vitest';
 import { transformMethod } from './transformMethod';
 
-describe('Named type-api import', () => {
+const MOCK_MODULE = {
     // language="javascript"
-    const code = // <editor-fold desc="Inline template">
-    `
-    import { defineMethod } from '@meteor-vite/type-api';
-    
-    export const createLink = defineMethod('links.create', {
-        handler: () => {
-            return {
-                myLink: { href: 'http://example.com' }
+    namedImport: `
+        // <editor-fold desc="Inline template">
+        import { defineMethod } from '@meteor-vite/type-api';
+        
+        export const createLink = defineMethod('links.create', {
+            handler: () => {
+                return {
+                    myLink: { href: 'http://example.com' }
+                }
             }
-        }
-    });
-    
-    export const updateLink = defineMethod('links.update', {
-        handler: (params) => {
-            return 'this should be omitted from client script';
-        }
-    })
-    `
+        });
+        
+        export const updateLink = defineMethod('links.update', {
+            handler: (params) => {
+                return 'this should be omitted from client script';
+            }
+        })
     // </editor-fold>
+    `
+}
+
+describe('Named type-api import', () => {
     
     it('Can parse ASTs', () => {
-        const { AST } = transformMethod(code);
+        const { AST } = transformMethod(MOCK_MODULE.namedImport);
         
         console.log(inspect(AST, { depth: 4, colors: true }));
         
         expect(AST).toBeTruthy();
     })
 })
+
+
