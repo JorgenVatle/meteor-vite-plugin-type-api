32c7310507b87bdd86a979874ea906913277ed2e
Jørgen Vatle <Jorgen@WcAServices.net>
2025-08-14 09:30:52 +0300

    Add transformMethod tests

---
+++ packages/type-api/src/plugin/transformMethod.test.ts
@@ -0,0 +1,27 @@
+import { expect, it } from 'vitest';
+import { transformMethod } from './transformMethod';
+
+// language="javascript"
+const code = `
+import { defineMethod } from '@meteor-vite/type-api';
+
+export const createLink = defineMethod('links.create', {
+    handler: () => {
+        return {
+            myLink: { href: 'http://example.com' }
+        }
+    }
+});
+
+export const updateLink = defineMethod('links.update', {
+    handler: (params) => {
+        return 'this should be omitted from client script';
+    }
+})
+`
+
+it('Can parse ASTs', () => {
+    const { AST } = transformMethod(code);
+    
+    expect(AST).toBeTruthy();
+})
