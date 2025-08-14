9380eb625a3b9a9458e95e358adf7c6c1febb25d
Jørgen Vatle <Jorgen@WcAServices.net>
2025-08-14 05:16:16 +0300

    Add utility function for defining build configs with shared options

--- packages/plugin-meteor-api-types/tsup.config.ts
+++ packages/plugin-meteor-api-types/tsup.config.ts
@@ -1,4 +1,26 @@
-import { defineConfig } from 'tsup';
+import { defineConfig, type Options } from 'tsup';
+
+function buildConfig(options: BuildConfigOptions): Options {
+    const BASE_OPTIONS = {
+        entry: ['./src/index.ts'],
+        format: ['esm', 'cjs'],
+        outDir: 'dist/node',
+        skipNodeModulesBundle: true,
+        dts: true,
+        clean: true,
+        define: {
+            Meteor: 'Meteor',
+            __IS_SERVER__: JSON.stringify(options.platform === 'node'),
+        }
+    } satisfies Options;
+    
+    Object.assign(BASE_OPTIONS.define, options.define);
+    return Object.assign(BASE_OPTIONS, options);
+}
+
+type BuildConfigOptions = Omit<Options, 'platform'> & {
+    platform: Extract<Options['platform'], string>
+};
 
 export default defineConfig([
     {
