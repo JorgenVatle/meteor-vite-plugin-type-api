0621adbad3703ba9f5616b98a8cec0881963210b
Jørgen Vatle <Jorgen@WcAServices.net>
2025-08-14 04:30:38 +0300

    Import API index in Vite entry

--- example/app/server/entry-vite.ts
+++ example/app/server/entry-vite.ts
@@ -1,5 +1,6 @@
 import { createLogger } from '@wcaservices/logger';
 import { Meteor } from 'meteor/meteor';
+import '../imports/api/index';
 
 const Logger = createLogger('App', {}, {
     msgPrefix: Meteor.isProduction ? '[PROD] ' : '[DEV] '
