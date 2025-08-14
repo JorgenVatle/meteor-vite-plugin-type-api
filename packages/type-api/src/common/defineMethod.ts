1ea1734d6f9840f38c6ebd09560eb7956f02f391
Jørgen Vatle <Jorgen@WcAServices.net>
2025-08-14 15:16:47 +0300

    Use resourceHandle helper for defining methods

--- packages/type-api/src/common/defineMethod.ts
+++ packages/type-api/src/common/defineMethod.ts
@@ -1,5 +1,6 @@
 import * as v from 'valibot';
 import { ApiNotCompiled } from '../lib/Errors/ApiNotCompiled';
+import { defineResourceHandle, getContext } from '../lib/ResourceHandle';
 
 export function defineMethod<
     TName extends string,
@@ -7,38 +8,24 @@ export function defineMethod<
     TSchemaInput,
     TSchemaTOutput,
 >(name: TName, definition: MethodDeclaration<TSchemaInput, TSchemaTOutput, TResult>) {
-    const label = __IS_SERVER__ ? `[Server Method: ${name}]` : `[Client Method: ${name}]`;
-    if (__IS_SERVER__) {
-        Meteor.methods({
-            [name]: (params: TSchemaInput) => {
-                try {
-                    console.debug(`${label} Incoming request: `, params);
-                    const schemaOutput = v.parse(definition.schema, params);
-                    return definition.method(schemaOutput);
-                } catch (error) {
-                    console.debug(`${label} Error: `, error);
-                    throw error;
-                }
-            }
-        });
-    } else {
-        throw new ApiNotCompiled(`Client Meteor API method has not been compiled yet! Make sure that the plugin is included in your Vite config and its named with a .methods.ts suffix or nested under a methods/ directory.`);
-    }
-    
-    console.debug(`${label} Defined`);
-    
-    return ((params: TSchemaInput): Promise<TResult> => {
-        return new Promise<TResult>((resolve, reject) => {
-            console.debug(`${label} Calling with params: `, params);
-            Meteor.call(name, params, (error: unknown, response: TResult) => {
-                if (error) {
-                    console.error(`${label} Error: `, error);
-                    return reject(error);
-                }
-                resolve(response);
-                console.debug(`${label} Response: `, response);
-            });
-        })
+    return defineResourceHandle({
+        name,
+        context: getContext(),
+        type: 'method',
+        schema: definition.schema,
+        run: (...params: [TSchemaInput]): Promise<TResult> => {
+            return new Promise<TResult>((resolve, reject) => {
+                Meteor.call(name, ...params, (error: unknown, response: TResult) => {
+                    if (error) {
+                        return reject(error);
+                    }
+                    resolve(response);
+                })
+            })
+        },
+        register: (handle) => {
+            Meteor.methods({ [name]: handle });
+        }
     });
 }
 
