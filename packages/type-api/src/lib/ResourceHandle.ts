5f850f90ae4cd7a7aa626f27b444a446f0904575
Jørgen Vatle <Jorgen@WcAServices.net>
2025-08-14 14:55:42 +0300

    Add universal resource handle registration approach

--- packages/type-api/src/lib/ResourceHandle.ts
+++ packages/type-api/src/lib/ResourceHandle.ts
@@ -1,17 +1,20 @@
+import * as v from 'valibot';
 import { ApiNotCompiled } from './Errors/ApiNotCompiled';
 
 export type ResourceHandle<
-    TParams extends any[] = [],
+    TInputParams extends any[] = [],
+    TOutputParams extends any[] = [],
     TResult = unknown
 > = {
     type: 'method' | 'publication';
     context: 'client' | 'server';
+    schema: v.GenericSchema<TOutputParams, TOutputParams>;
     name: string;
-    run: (...params: TParams) => TResult;
-    register: (label: string) => void;
+    call: (...params: TInputParams) => TResult;
+    handle: (...params: TOutputParams) => TResult;
 };
 
-export function getLabel(handle: ResourceHandle) {
+export function getLabel(handle: ResourceHandle): string {
     return `[${handle.context} ${handle.type}] ${handle.name}`;
 }
 
@@ -20,24 +23,53 @@ export function getContext() {
 }
 
 export function defineResourceHandle<
-    TParams extends any[] = [],
+    TInputParams extends any[] = [],
+    TOutputParams extends any[] = [],
     TResult = unknown
->(handle: ResourceHandle<TParams, TResult>) {
+>(handle: ResourceHandle<TInputParams, TOutputParams, TResult>) {
     const label = getLabel(handle);
     
-    if (__IS_SERVER__) {
-        handle.register(label);
-    } else {
+    if (!__IS_SERVER__) {
         throw new ApiNotCompiled(`Client Meteor API method has not been compiled yet! Make sure that the plugin is included in your Vite config and its named with a .methods.ts suffix or nested under a methods/ directory.`);
     }
     
+    function wrappedHandle(this: any, params: TOutputParams): any {
+        try {
+            console.debug(`${label} Incoming request: `, params);
+            const schemaOutput = v.parse(handle.schema, params);
+            return handle.handle.apply(this, schemaOutput);
+        } catch (error) {
+            console.debug(`${label} Error: `, error);
+            throw error;
+        }
+    }
+    
+    if (handle.type === 'method') {
+        Meteor.methods({
+            [handle.name]: wrappedHandle,
+        });
+    } else if (handle.type === 'publication') {
+        Meteor.publish(handle.name, wrappedHandle);
+    }
+    
     console.debug(`${label} Defined resource handle`);
-    return (async (...params: TParams): Promise<TResult> => {
+    return (async (...params: TInputParams): Promise<TResult> => {
         try {
             console.debug(`${label} Calling with params: `, params);
-            const result = await handle.run(...params);
-            console.debug(`${label} Response: `, result);
-            return result;
+            if (handle.type === 'method') {
+                const result = await new Promise<TResult>((resolve, reject) => {
+                    Meteor.call(handle.name, ...params, (error: unknown, response: TResult) => {
+                        if (error) {
+                            return reject(error);
+                        }
+                        resolve(response);
+                    });
+                })
+                console.debug(`${label} Response: `, result);
+                return result;
+            }
+            
+            return Meteor.subscribe(handle.name, ...params);
         } catch (error) {
             console.error(`${label} Error: `, error);
             throw error;
