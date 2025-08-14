9491f6863f587015e366428de77b17bbee92b7eb
Jørgen Vatle <Jorgen@WcAServices.net>
2025-08-14 06:52:56 +0300

    Add name and file extension config options

--- packages/type-api/src/plugin/index.ts
+++ packages/type-api/src/plugin/index.ts
@@ -1,11 +1,53 @@
 import { type Plugin } from 'vite';
 
-export default function meteorApiTypePlugin(config?: PluginConfiguration): Plugin {
+export default function meteorApiTypePlugin(config: PluginConfiguration): Plugin {
     return {
         name: 'meteor-vite: meteor-api-types',
     }
 }
 
 export interface PluginConfiguration {
-
+    methods?: {
+        /**
+         * Parent directory for Meteor methods.
+         *
+         * All files within directories matching this name will be treated as
+         * Meteor methods regardless of their file extension.
+         *
+         * @default methods
+         */
+        dirname?: string;
+        
+        /**
+         * File extension for Meteor methods.
+         *
+         * This can be used as an alternative to nesting methods under a
+         * methods directory.
+         *
+         * @default .methods.ts
+         */
+        fileExtension?: string;
+    },
+    
+    publications?: {
+        /**
+         * Parent directory for Meteor publications.
+         *
+         * All files within directories matching this name will be treated as
+         * Meteor publications regardless of their file extension.
+         *
+         * @default publications
+         */
+        dirname?: string;
+        
+        /**
+         * File extension for Meteor publications.
+         *
+         * This can be used as an alternative to nesting publications under a
+         * publications directory.
+         *
+         * @default .publications.ts
+         */
+        fileExtension?: string;
+    }
 }
