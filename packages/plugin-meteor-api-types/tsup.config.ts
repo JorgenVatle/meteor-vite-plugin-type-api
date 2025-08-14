3ceb1e6eafaf776dce890fe1adb6781f9745f1d1
Jørgen Vatle <Jorgen@WcAServices.net>
2025-08-14 05:33:27 +0300

    Require names to be specified for build configs

--- packages/plugin-meteor-api-types/tsup.config.ts
+++ packages/plugin-meteor-api-types/tsup.config.ts
@@ -2,33 +2,43 @@ import { defineConfig, type Options } from 'tsup';
 
 function buildConfig(options: BuildConfigOptions): Options {
     const BASE_OPTIONS = {
-        name: options.platform.toUpperCase(),
-        entry: ['./src/index.ts'],
         format: ['esm', 'cjs'],
-        outDir: `dist/${options.platform}`,
         skipNodeModulesBundle: true,
         dts: true,
         clean: true,
         define: {
             Meteor: 'Meteor',
-            __IS_SERVER__: JSON.stringify(options.platform === 'node'),
         }
     } satisfies Options;
     
-    Object.assign(BASE_OPTIONS.define, options.define);
-    return Object.assign(BASE_OPTIONS, options);
+    const overrides = {
+        name: `${options.platform.toUpperCase()}: ${options.name}`,
+        outDir: `dist/${options.platform}`,
+        define: {
+            __IS_SERVER__: JSON.stringify(options.platform === 'node'),
+        },
+    } satisfies Options;
+    
+    return Object.assign({}, options, overrides, {
+        define: Object.assign(BASE_OPTIONS.define, options.define, overrides.define),
+    });
 }
-
-type BuildConfigOptions = Omit<Options, 'platform'> & {
-    platform: Extract<Options['platform'], string>;
-};
 
 export default defineConfig([
     buildConfig({
+        name: 'index',
         platform: 'node',
     }),
     buildConfig({
+        name: 'index',
         platform: 'browser',
         treeshake: 'smallest',
     }),
 ])
+
+interface BuildConfigBaseOptions {
+    name: string;
+    platform: Extract<Options['platform'], string>;
+}
+
+interface BuildConfigOptions extends BuildConfigBaseOptions, Omit<Options, keyof BuildConfigBaseOptions> {}
