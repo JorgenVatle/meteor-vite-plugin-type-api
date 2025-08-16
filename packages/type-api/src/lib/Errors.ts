2e98047eb91ed868d14e867c98f7803c925026ad
Jørgen Vatle <Jorgen@WcAServices.net>
2025-08-16 16:08:59 +0300

    Add InvalidEnvironment error

--- packages/type-api/src/lib/Errors.ts
+++ packages/type-api/src/lib/Errors.ts
@@ -7,3 +7,5 @@ export class ClientOnly extends ApiTypeError {}
 export class ApiTransformError extends ApiTypeError {}
 
 export class InvalidApiDefinition extends ApiTypeError {}
+
+export class InvalidEnvironment extends ApiTypeError {}
