ef942118a49df7af5c97a7aad2ba2cf28aed0333
Jørgen Vatle <Jorgen@WcAServices.net>
2025-08-16 15:42:40 +0300

    Update defineMethod usage

--- example/app/imports/api/links.methods.ts
+++ example/app/imports/api/links.methods.ts
@@ -2,9 +2,8 @@ import { defineMethod } from '@meteor-vite/type-api';
 import { LinkCollection, LinkDocumentSchema } from './links.collection';
 
 export const createLink = defineMethod({
-    name: 'links.create',
     schema: LinkDocumentSchema,
-    method: (link) => {
+    run: (link) => {
         return LinkCollection.insertAsync(link);
     }
 });
