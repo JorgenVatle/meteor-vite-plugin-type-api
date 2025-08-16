56d049ef73e89feac3fa1a12b51beedcffa5e566
Jørgen Vatle <Jorgen@WcAServices.net>
2025-08-16 15:02:47 +0300

    Replace defineMethod module with simplified common index module

--- packages/type-api/src/common/index.ts
+++ packages/type-api/src/common/index.ts
@@ -1,1 +1,15 @@
-export * from './defineMethod';
+import type { ResourceConfig, ResourceHandle } from '@/lib/ResourceConfig';
+
+export function defineMethod<
+    TParams extends any[],
+    TResult
+>(config: ResourceConfig<TResult, TParams>): ResourceHandle<Promise<TResult>, TParams> {
+    throw new Error('Method has not been transformed by Meteor-Vite!');
+}
+
+export function definePublication<
+    TParams extends any[],
+    TResult
+>(config: ResourceConfig<TResult, TParams>): ResourceHandle<Meteor.SubscriptionHandle, TParams> {
+    throw new Error('Publication has not been transformed by Meteor-Vite!');
+}
