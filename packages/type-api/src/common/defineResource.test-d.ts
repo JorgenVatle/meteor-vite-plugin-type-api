5b5e2e845c336700bfda6d5d8de75efec12038ac
Jørgen Vatle <Jorgen@WcAServices.net>
2025-08-16 15:15:31 +0300

    Add defineMethod type tests

---
+++ packages/type-api/src/common/defineResource.test-d.ts
@@ -0,0 +1,21 @@
+import { defineMethod } from '@/common/defineResource';
+import * as v from 'valibot';
+import { describe, expectTypeOf, it } from 'vitest';
+
+describe('defineMethod', () => {
+    const schema = v.object({
+        href: v.string(),
+        title: v.string(),
+    });
+    
+    it('will infer run() method parameters', () => {
+        defineMethod({
+            schema,
+            run: ({ href, title }) => {
+                expectTypeOf(href).toEqualTypeOf<string>();
+                expectTypeOf(title).toEqualTypeOf<string>();
+            }
+        })
+    });
+    
+})
