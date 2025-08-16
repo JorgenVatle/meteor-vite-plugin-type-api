fa7668bb3ed191e69ade57ff75d9c73124620389
Jørgen Vatle <Jorgen@WcAServices.net>
2025-08-16 16:46:50 +0300

    Correct definition type for definePublication

--- packages/type-api/src/entry/server/defineServerResource.ts
+++ packages/type-api/src/entry/server/defineServerResource.ts
@@ -15,11 +15,11 @@ export function defineMethod(config: ResourceDefinitionConfig) {
 }
 
 export function definePublication(config: ResourceDefinitionConfig) {
-    const resource = new ResourceDefinition('method', config);
+    const resource = new ResourceDefinition('publication', config);
     
     Meteor.publish(resource.name, resource.requestHandle());
     
     return resource.callHandle(() => {
-        throw new ClientOnly('This method can only be called on the client')
+        throw new ClientOnly('This publication hook can only be called on the client')
     });
 }
