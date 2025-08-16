070f77bd1e39907ae83c74592cb7e9a227630a1e
Jørgen Vatle <Jorgen@WcAServices.net>
2025-08-16 16:23:27 +0300

    Add additional properties to resource definition class

--- packages/type-api/src/lib/ResourceDefinition.ts
+++ packages/type-api/src/lib/ResourceDefinition.ts
@@ -1,15 +1,27 @@
-export class ResourceDefinition {
+import type * as v from 'valibot';
+
+export class ResourceDefinition implements ResourceDefinitionConfig {
     public readonly type: ResourceType;
     public readonly environment: ResourceEnvironment;
+    public readonly name: string;
+    public readonly schema: v.GenericSchema;
+    public readonly run: ResourceDefinitionConfig['run'];
+    
     constructor(config: ResourceDefinitionConfig) {
         this.type = config.type;
         this.environment = config.environment;
+        this.name = config.name;
+        this.schema = config.schema;
+        this.run = config.run;
     }
 }
 
 interface ResourceDefinitionConfig {
+    name: string;
     type: ResourceType;
+    schema: v.GenericSchema;
     environment: ResourceEnvironment;
+    run: (...params: any) => any;
 }
 
 export type ResourceType = 'method' | 'publication';
