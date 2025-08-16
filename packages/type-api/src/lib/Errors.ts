8eb3f9b4617b97bca62950d849c1524dca4f0205
Jørgen Vatle <Jorgen@WcAServices.net>
2025-08-16 16:07:45 +0300

    Add ApiTransformError class

--- packages/type-api/src/lib/Errors.ts
+++ packages/type-api/src/lib/Errors.ts
@@ -3,3 +3,5 @@ export class ApiTypeError extends Error {}
 export class ApiNotCompiled extends ApiTypeError {}
 
 export class ClientOnly extends ApiTypeError {}
+
+export class ApiTransformError extends ApiTypeError {}
