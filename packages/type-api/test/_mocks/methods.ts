b7a72c3c9416a2ff35dfd223ee492fcfb486f4f2
Jørgen Vatle <Jorgen@WcAServices.net>
2025-08-16 19:11:32 +0300

    Temporarily disable non-named import mocks

--- packages/type-api/test/_mocks/methods.ts
+++ packages/type-api/test/_mocks/methods.ts
@@ -21,44 +21,44 @@ export const MOCK_MODULE_METHODS = {
     `,
     
     // language="javascript"
-    'wildcard import': `
-        import * as TypeApi from '@meteor-vite/type-api';
-        
-        export const createLink = TypeApi.defineMethod({
-            name: 'links.create',
-            method: () => {
-                return {
-                    myLink: { href: 'http://example.com' }
-                }
-            }
-        });
-        
-        export const updateLink = TypeApi.defineMethod({
-            name: 'links.update',
-            method: (params) => {
-                return 'this should be omitted from client script';
-            }
-        })
-    `,
-    
-    // language="javascript"
-    'default import': `
-        import TypeApi from '@meteor-vite/type-api';
-        
-        export const createLink = TypeApi.defineMethod({
-            name: 'links.create',
-            method: () => {
-                return {
-                    myLink: { href: 'http://example.com' }
-                }
-            }
-        });
-        
-        export const updateLink = TypeApi.defineMethod({
-            name: 'links.update',
-            method: (params) => {
-                return 'this should be omitted from client script';
-            }
-        })
-    `,
+    // 'wildcard import': `
+    //     import * as TypeApi from '@meteor-vite/type-api';
+    //
+    //     export const createLink = TypeApi.defineMethod({
+    //         name: 'links.create',
+    //         method: () => {
+    //             return {
+    //                 myLink: { href: 'http://example.com' }
+    //             }
+    //         }
+    //     });
+    //
+    //     export const updateLink = TypeApi.defineMethod({
+    //         name: 'links.update',
+    //         method: (params) => {
+    //             return 'this should be omitted from client script';
+    //         }
+    //     })
+    // `,
+    //
+    // // language="javascript"
+    // 'default import': `
+    //     import TypeApi from '@meteor-vite/type-api';
+    //
+    //     export const createLink = TypeApi.defineMethod({
+    //         name: 'links.create',
+    //         method: () => {
+    //             return {
+    //                 myLink: { href: 'http://example.com' }
+    //             }
+    //         }
+    //     });
+    //
+    //     export const updateLink = TypeApi.defineMethod({
+    //         name: 'links.update',
+    //         method: (params) => {
+    //             return 'this should be omitted from client script';
+    //         }
+    //     })
+    // `,
 }
