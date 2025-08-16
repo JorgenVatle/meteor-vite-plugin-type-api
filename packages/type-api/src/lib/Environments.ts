69c8b9cd58c04c31738754909a859e63a9dfeaaf
Jørgen Vatle <Jorgen@WcAServices.net>
2025-08-16 18:47:04 +0300

    Remove unused resourceLabel function

--- packages/type-api/src/lib/Environments.ts
+++ packages/type-api/src/lib/Environments.ts
@@ -1,5 +1,4 @@
 import { InvalidEnvironment } from '@/lib/Errors';
-import type { InternalResourceConfig } from '@/lib/ResourceConfig';
 
 export const SUPPORTED_ENVIRONMENTS = [
     'server',
@@ -12,11 +11,6 @@ export type TargetEnvironment = SupportedEnvironment | ({} & string);
 
 export function isSupportedEnvironment(target: TargetEnvironment) {
     return SUPPORTED_ENVIRONMENTS.some(name => name === target);
-}
-
-export type ResourceType = 'method' | 'publication';
-export function resourceLabel(type: ResourceType, config: InternalResourceConfig): string {
-    return `(${config._environment.toUpperCase()}) [${type}: ${config.name || config._defaultName}]`;
 }
 
 export const ENTRY_MODULE: Record<TargetEnvironment, string> = {
